c7fbeaba31ea8a69ad12b02b3dc4948b62f16083
Priyanshi vishwakarma <priyanshivishwakarma201@gmail.com>
2026-04-05 11:31:08 +0530

    Improve readability and add comments in smaller.c
    Refactor code for clarity and add comments.

--- smaller.c
+++ smaller.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
-int main(){
-int a,b;
-printf("enter two values:");
-scanf("%d%d", &a, &b);
+#include <stdio.h>   // Header file for input and output functions
 
-if(a<b){
-    printf("%d is less than %d", a,b);
-}
-else{
-    printf("%d is less than %d", b,a);
-}
- return 0;
+int main() {
+
+    int a, b;   // Declare two integer variables
+
+    // Ask user to enter two values
+    printf("Enter two values: ");
+
+    // Take input from user and store in variables a and b
+    scanf("%d%d", &a, &b);
+
+    // Check if a is less than b
+    if(a < b) {
+        // If condition is true, print that a is smaller
+        printf("%d is less than %d", a, b);
+    }
+    else {
+        // If condition is false, print that b is smaller
+        printf("%d is less than %d", b, a);
+    }
+
+    return 0;   // End of program
 }
